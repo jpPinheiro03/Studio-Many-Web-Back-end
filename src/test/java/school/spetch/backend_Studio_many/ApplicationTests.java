64a27deb64e7ed5ea043ae9d6daa04dc0ea3a035
jpPinheiro03 <joao.pinheiro@sptech.school>
2026-03-17 18:30:33 -0300

    criando o projeto com as dependencias

---
+++ src/test/java/school/spetch/backend_Studio_many/ApplicationTests.java
@@ -0,0 +1,13 @@
+package school.spetch.backend_Studio_many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
