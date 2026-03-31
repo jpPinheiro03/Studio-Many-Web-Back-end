5a61b28a379297f0e6990817ac461970f1718911
Rafael-Mechi <rafael.doliveira@sptech.school>
2026-03-30 23:56:50 -0300

    adicionando doc do endpoint de bloqueio

--- src/main/java/com/studio/core/dominio/bloqueio/dto/BloqueioRequestDTO.java
+++ src/main/java/com/studio/core/dominio/bloqueio/dto/BloqueioRequestDTO.java
@@ -1,5 +1,6 @@
 package com.studio.core.dominio.bloqueio.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -8,14 +9,18 @@ import java.time.LocalDateTime;
 public class BloqueioRequestDTO {
 
     @NotNull(message = "Funcionário ID é obrigatório")
+    @Schema(name = "funcionarioId", example = "1")
     private Long funcionarioId;
 
     @NotNull(message = "Data de início é obrigatória")
+    @Schema(name = "dataInicio", example = "2026-03-29T20:36:32.949Z")
     private LocalDateTime dataInicio;
 
     @NotNull(message = "Data de fim é obrigatória")
+    @Schema(name = "dataFim", example = "2026-06-29T20:36:32.949Z")
     private LocalDateTime dataFim;
 
+    @Schema(name = "motivo", example = "Férias")
     private String motivo;
 
     public Long getFuncionarioId() {
